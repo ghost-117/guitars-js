16f81016c0d835ff8075ae4d58ca839ab4962503
Jonh Look <Axel Lopez Cornejo>
2025-10-16 11:12:57 -0600

    03 Contruye carrito

--- GuitarsApp/js/app.js
+++ GuitarsApp/js/app.js
@@ -1,5 +1,6 @@
 import { db} from "./guitarras.js"
 
+const Carrito = []
 const divContainer = document.querySelector('main div')
 
 const createCard = (guitar) => {
@@ -11,14 +12,38 @@ const createCard = (guitar) => {
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">${ guitar.nombre }</h3>
                     <p>${guitar.descripcion}</p>
-                    <p class="fw-black text-primary fs-3">${guitar.precio}</p>
+                    <p class="fw-black text-primary fs-3">$${guitar.precio}</p>
                     <button 
+                        data-id="${guitar.id}"
                         type="button"
                         class="btn btn-dark w-100 "
                     >Agregar al Carrito</button>
                 </div>`
     div.innerHTML = html    
     return div
+}
+const buttonClicked = (e) => {
+    if(e.target.classList.contains('btn')){
+        const dataId = e.target.getAttribute('data-id')
+        //Verificar si existe guitar en carrito
+        const idCarrito = Carrito.findIndex(g => g.id === Number(dataId))
+        //Si no, crea un objeto nuevo
+        if(idCarrito === -1){
+            Carrito.push({
+                ...db[Number(dataId) - 1 ],
+                cantidad: 1
+            })
+        } else { 
+            //Si si, incrementa cantidad
+            Carrito[idCarrito].cantidad++
+        }
+        
+        Carrito.push({
+            ...db[Number(dataId) -1 ],
+            cantidad : 1
+        })
+        console.log(Carrito)
+    }
 }
 //Iterar arrays
 // ciclos
@@ -31,3 +56,4 @@ db.forEach((guitar) => {
     console.log(guitar.nombre)
     divContainer.appendChild(createCard(guitar))
 })
+divContainer.addEventListener('click', buttonClicked)
